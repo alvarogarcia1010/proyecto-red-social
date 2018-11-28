f283684dfd245663ba39406061037b46f150d64e
feranstirman1 <00024117@uca.edu.sv>
2018-11-28 14:33:07 -0600

    se quito el connect-multiparty

--- src/Routes/publicacion.js
+++ src/Routes/publicacion.js
@@ -5,8 +5,8 @@ var publicationController=require('../Controllers/Publicacion');
 var api=express.Router();
 var md_auth=require('../Middlewares/AuthMiddleware');
 
-var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir:'./Uploads/Publications'});
+//var multipart = require('connect-multiparty');
+//var md_upload = multipart({uploadDir:'./Uploads/Publications'});
 
 api.get('/',function(req,res,next){
     res.render('dashboard');
